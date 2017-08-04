c97a4a5eeded7d156bb0722b4375388ccecd3e4a
Paris Pantigoso <proinnovaperu@outlook.com>
2017-08-04 16:49:29 -0500

    SOAP reader and deserialization

---
+++ ConsoleSOAP/Class1.cs
@@ -0,0 +1,257 @@
+﻿/* 
+ Licensed under the Apache License, Version 2.0
+
+ http://www.apache.org/licenses/LICENSE-2.0
+ */
+using System;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+
+namespace Xml2CSharp
+{
+    [XmlRoot(ElementName = "BuildingId", Namespace = "http://tempuri.org/")]
+    public class BuildingId
+    {
+        [XmlAttribute(AttributeName = "nil", Namespace = "http://www.w3.org/2001/XMLSchema-instance")]
+        public string Nil { get; set; }
+    }
+
+    [XmlRoot(ElementName = "CaseType", Namespace = "http://tempuri.org/")]
+    public class CaseType
+    {
+        [XmlAttribute(AttributeName = "nil", Namespace = "http://www.w3.org/2001/XMLSchema-instance")]
+        public string Nil { get; set; }
+    }
+
+    [XmlRoot(ElementName = "ChargeId", Namespace = "http://tempuri.org/")]
+    public class ChargeId
+    {
+        [XmlAttribute(AttributeName = "nil", Namespace = "http://www.w3.org/2001/XMLSchema-instance")]
+        public string Nil { get; set; }
+    }
+
+    [XmlRoot(ElementName = "CityId", Namespace = "http://tempuri.org/")]
+    public class CityId
+    {
+        [XmlAttribute(AttributeName = "nil", Namespace = "http://www.w3.org/2001/XMLSchema-instance")]
+        public string Nil { get; set; }
+    }
+
+    [XmlRoot(ElementName = "CodeDepto", Namespace = "http://tempuri.org/")]
+    public class CodeDepto
+    {
+        [XmlAttribute(AttributeName = "nil", Namespace = "http://www.w3.org/2001/XMLSchema-instance")]
+        public string Nil { get; set; }
+    }
+
+    [XmlRoot(ElementName = "CodeInmediateBoss", Namespace = "http://tempuri.org/")]
+    public class CodeInmediateBoss
+    {
+        [XmlAttribute(AttributeName = "nil", Namespace = "http://www.w3.org/2001/XMLSchema-instance")]
+        public string Nil { get; set; }
+    }
+
+    [XmlRoot(ElementName = "CodeProfile", Namespace = "http://tempuri.org/")]
+    public class CodeProfile
+    {
+        [XmlAttribute(AttributeName = "nil", Namespace = "http://www.w3.org/2001/XMLSchema-instance")]
+        public string Nil { get; set; }
+    }
+
+    [XmlRoot(ElementName = "CompanyId", Namespace = "http://tempuri.org/")]
+    public class CompanyId
+    {
+        [XmlAttribute(AttributeName = "nil", Namespace = "http://www.w3.org/2001/XMLSchema-instance")]
+        public string Nil { get; set; }
+    }
+
+    [XmlRoot(ElementName = "CountryId", Namespace = "http://tempuri.org/")]
+    public class CountryId
+    {
+        [XmlAttribute(AttributeName = "nil", Namespace = "http://www.w3.org/2001/XMLSchema-instance")]
+        public string Nil { get; set; }
+    }
+
+    [XmlRoot(ElementName = "ExpirationDate", Namespace = "http://tempuri.org/")]
+    public class ExpirationDate
+    {
+        [XmlAttribute(AttributeName = "nil", Namespace = "http://www.w3.org/2001/XMLSchema-instance")]
+        public string Nil { get; set; }
+    }
+
+    [XmlRoot(ElementName = "FloorId", Namespace = "http://tempuri.org/")]
+    public class FloorId
+    {
+        [XmlAttribute(AttributeName = "nil", Namespace = "http://www.w3.org/2001/XMLSchema-instance")]
+        public string Nil { get; set; }
+    }
+
+    [XmlRoot(ElementName = "Level", Namespace = "http://tempuri.org/")]
+    public class Level
+    {
+        [XmlAttribute(AttributeName = "nil", Namespace = "http://www.w3.org/2001/XMLSchema-instance")]
+        public string Nil { get; set; }
+    }
+
+    [XmlRoot(ElementName = "KnowledgeLevel", Namespace = "http://tempuri.org/")]
+    public class KnowledgeLevel
+    {
+        [XmlAttribute(AttributeName = "nil", Namespace = "http://www.w3.org/2001/XMLSchema-instance")]
+        public string Nil { get; set; }
+    }
+
+    [XmlRoot(ElementName = "NumberDepartmentId", Namespace = "http://tempuri.org/")]
+    public class NumberDepartmentId
+    {
+        [XmlAttribute(AttributeName = "nil", Namespace = "http://www.w3.org/2001/XMLSchema-instance")]
+        public string Nil { get; set; }
+    }
+
+    [XmlRoot(ElementName = "ProfileId", Namespace = "http://tempuri.org/")]
+    public class ProfileId
+    {
+        [XmlAttribute(AttributeName = "nil", Namespace = "http://www.w3.org/2001/XMLSchema-instance")]
+        public string Nil { get; set; }
+    }
+
+    [XmlRoot(ElementName = "SiteId", Namespace = "http://tempuri.org/")]
+    public class SiteId
+    {
+        [XmlAttribute(AttributeName = "nil", Namespace = "http://www.w3.org/2001/XMLSchema-instance")]
+        public string Nil { get; set; }
+    }
+
+    [XmlRoot(ElementName = "Ula", Namespace = "http://tempuri.org/")]
+    public class Ula
+    {
+        [XmlAttribute(AttributeName = "nil", Namespace = "http://www.w3.org/2001/XMLSchema-instance")]
+        public string Nil { get; set; }
+    }
+
+    [XmlRoot(ElementName = "UserActive", Namespace = "http://tempuri.org/")]
+    public class UserActive
+    {
+        [XmlAttribute(AttributeName = "nil", Namespace = "http://www.w3.org/2001/XMLSchema-instance")]
+        public string Nil { get; set; }
+    }
+
+    [XmlRoot(ElementName = "UserBirthDate", Namespace = "http://tempuri.org/")]
+    public class UserBirthDate
+    {
+        [XmlAttribute(AttributeName = "nil", Namespace = "http://www.w3.org/2001/XMLSchema-instance")]
+        public string Nil { get; set; }
+    }
+
+    [XmlRoot(ElementName = "UserType", Namespace = "http://tempuri.org/")]
+    public class UserType
+    {
+        [XmlAttribute(AttributeName = "nil", Namespace = "http://www.w3.org/2001/XMLSchema-instance")]
+        public string Nil { get; set; }
+    }
+
+    [XmlRoot(ElementName = "UssId", Namespace = "http://tempuri.org/")]
+    public class UssId
+    {
+        [XmlAttribute(AttributeName = "nil", Namespace = "http://www.w3.org/2001/XMLSchema-instance")]
+        public string Nil { get; set; }
+    }
+
+    [XmlRoot(ElementName = "ProjectId", Namespace = "http://tempuri.org/")]
+    public class ProjectId
+    {
+        [XmlAttribute(AttributeName = "nil", Namespace = "http://www.w3.org/2001/XMLSchema-instance")]
+        public string Nil { get; set; }
+    }
+
+    [XmlRoot(ElementName = "User", Namespace = "http://tempuri.org/")]
+    public class User
+    {
+        [XmlElement(ElementName = "BuildingId", Namespace = "http://tempuri.org/")]
+        public BuildingId BuildingId { get; set; }
+        [XmlElement(ElementName = "CaseType", Namespace = "http://tempuri.org/")]
+        public CaseType CaseType { get; set; }
+        [XmlElement(ElementName = "ChargeId", Namespace = "http://tempuri.org/")]
+        public ChargeId ChargeId { get; set; }
+        [XmlElement(ElementName = "CityId", Namespace = "http://tempuri.org/")]
+        public CityId CityId { get; set; }
+        [XmlElement(ElementName = "CodeDepto", Namespace = "http://tempuri.org/")]
+        public CodeDepto CodeDepto { get; set; }
+        [XmlElement(ElementName = "CodeInmediateBoss", Namespace = "http://tempuri.org/")]
+        public CodeInmediateBoss CodeInmediateBoss { get; set; }
+        [XmlElement(ElementName = "CodeProfile", Namespace = "http://tempuri.org/")]
+        public CodeProfile CodeProfile { get; set; }
+        [XmlElement(ElementName = "CodeUser", Namespace = "http://tempuri.org/")]
+        public string CodeUser { get; set; }
+        [XmlElement(ElementName = "CompanyId", Namespace = "http://tempuri.org/")]
+        public CompanyId CompanyId { get; set; }
+        [XmlElement(ElementName = "CountryId", Namespace = "http://tempuri.org/")]
+        public CountryId CountryId { get; set; }
+        [XmlElement(ElementName = "ExpirationDate", Namespace = "http://tempuri.org/")]
+        public ExpirationDate ExpirationDate { get; set; }
+        [XmlElement(ElementName = "FloorId", Namespace = "http://tempuri.org/")]
+        public FloorId FloorId { get; set; }
+        [XmlElement(ElementName = "Level", Namespace = "http://tempuri.org/")]
+        public Level Level { get; set; }
+        [XmlElement(ElementName = "KnowledgeLevel", Namespace = "http://tempuri.org/")]
+        public KnowledgeLevel KnowledgeLevel { get; set; }
+        [XmlElement(ElementName = "NumberDepartmentId", Namespace = "http://tempuri.org/")]
+        public NumberDepartmentId NumberDepartmentId { get; set; }
+        [XmlElement(ElementName = "ProfileId", Namespace = "http://tempuri.org/")]
+        public ProfileId ProfileId { get; set; }
+        [XmlElement(ElementName = "SiteId", Namespace = "http://tempuri.org/")]
+        public SiteId SiteId { get; set; }
+        [XmlElement(ElementName = "Ula", Namespace = "http://tempuri.org/")]
+        public Ula Ula { get; set; }
+        [XmlElement(ElementName = "Uname", Namespace = "http://tempuri.org/")]
+        public string Uname { get; set; }
+        [XmlElement(ElementName = "UserActive", Namespace = "http://tempuri.org/")]
+        public UserActive UserActive { get; set; }
+        [XmlElement(ElementName = "UserBirthDate", Namespace = "http://tempuri.org/")]
+        public UserBirthDate UserBirthDate { get; set; }
+        [XmlElement(ElementName = "UserType", Namespace = "http://tempuri.org/")]
+        public UserType UserType { get; set; }
+        [XmlElement(ElementName = "UssId", Namespace = "http://tempuri.org/")]
+        public UssId UssId { get; set; }
+        [XmlElement(ElementName = "ProjectId", Namespace = "http://tempuri.org/")]
+        public ProjectId ProjectId { get; set; }
+        [XmlElement(ElementName = "Role", Namespace = "http://tempuri.org/")]
+        public string Role { get; set; }
+    }
+
+    [XmlRoot(ElementName = "ListPerfilesResult", Namespace = "http://tempuri.org/")]
+    public class ListPerfilesResult
+    {
+        [XmlElement(ElementName = "User", Namespace = "http://tempuri.org/")]
+        public List<User> User { get; set; }
+    }
+
+    [XmlRoot(ElementName = "ListPerfilesResponse", Namespace = "http://tempuri.org/")]
+    public class ListPerfilesResponse
+    {
+        [XmlElement(ElementName = "ListPerfilesResult", Namespace = "http://tempuri.org/")]
+        public ListPerfilesResult ListPerfilesResult { get; set; }
+        [XmlAttribute(AttributeName = "xmlns")]
+        public string Xmlns { get; set; }
+    }
+
+    [XmlRoot(ElementName = "Body", Namespace = "http://schemas.xmlsoap.org/soap/envelope/")]
+    public class Body
+    {
+        [XmlElement(ElementName = "ListPerfilesResponse", Namespace = "http://tempuri.org/")]
+        public ListPerfilesResponse ListPerfilesResponse { get; set; }
+    }
+
+    [XmlRoot(ElementName = "Envelope", Namespace = "http://schemas.xmlsoap.org/soap/envelope/")]
+    public class Envelope
+    {
+        [XmlElement(ElementName = "Body", Namespace = "http://schemas.xmlsoap.org/soap/envelope/")]
+        public Body Body { get; set; }
+        [XmlAttribute(AttributeName = "soap", Namespace = "http://www.w3.org/2000/xmlns/")]
+        public string Soap { get; set; }
+        [XmlAttribute(AttributeName = "xsi", Namespace = "http://www.w3.org/2000/xmlns/")]
+        public string Xsi { get; set; }
+        [XmlAttribute(AttributeName = "xsd", Namespace = "http://www.w3.org/2000/xmlns/")]
+        public string Xsd { get; set; }
+    }
+
+}
--- ConsoleSOAP/Program.cs
+++ ConsoleSOAP/Program.cs
@@ -5,71 +5,160 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using Xml2CSharp;
 
 namespace ConsoleSOAP
 {
     class Program
     {
-        public static void Main(string[] args)
+        //public static void Main(string[] args)
+        //{
+        //    // OK, this is not a WCF web service, but that should not matter :D
+        //    //string endpoint = "http://www.html2xml.nl/Services/Calculator/Version1/Calculator.asmx";
+        //    string endpoint = "http://172.36.0.141/asdkws/Calculator.asmx";
+
+        //    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(endpoint);
+
+        //    request.ContentType = "text/xml"; // or application/soap+xml for SOAP 1.2
+        //    request.Method = "POST";
+        //    request.KeepAlive = false;
+
+        //    ////In case you have a proxy to resolve the server name also add these lines
+        //    //var proxyServer = new WebProxy("XX.XX.XX.XX", 1234);
+
+        //    //proxyServer.Credentials = CredentialCache.DefaultCredentials; // or username + password
+
+        //    //request.Proxy = proxyServer;
+
+        //    // you can read these from files
+        //    string payload = @"<soapenv:Envelope xmlns:soapenv=""http://schemas.xmlsoap.org/soap/envelope/"" xmlns:tem=""http://tempuri.org/"">
+        //                       <soapenv:Header/>
+        //                       <soapenv:Body>
+        //                          <tem:Add>
+        //                             <tem:a>1</tem:a>
+        //                             <tem:b>2</tem:b>
+        //                          </tem:Add>
+        //                       </soapenv:Body>
+        //                    </soapenv:Envelope>";
+
+        //    byte[] byteArray = Encoding.UTF8.GetBytes(payload);
+
+        //    request.ContentLength = byteArray.Length;
+
+        //    Stream requestStream = request.GetRequestStream();
+
+        //    requestStream.Write(byteArray, 0, byteArray.Length);
+        //    requestStream.Close();
+
+        //    HttpWebResponse response = null;
+        //    try
+        //    {
+        //        response = (HttpWebResponse)request.GetResponse();
+        //    }
+        //    catch (WebException ex)
+        //    {
+        //        response = (HttpWebResponse)ex.Response;
+        //    }
+
+        //    Console.WriteLine(string.Format("HTTP/{0} {1} {2}\n", response.ProtocolVersion, (int)response.StatusCode, response.StatusDescription));
+
+        //    // you can write this to files
+        //    Stream responseStream = response.GetResponseStream();
+        //    StreamReader reader = new StreamReader(responseStream);
+        //    Console.WriteLine(reader.ReadToEnd());
+
+        //    // cleanp
+        //    reader.Close();
+        //    requestStream.Close();
+        //    responseStream.Close();
+        //    response.Close();
+        //}
+
+
+        static void Main(string[] args)
         {
-            // OK, this is not a WCF web service, but that should not matter :D
-            string endpoint = "http://www.html2xml.nl/Services/Calculator/Version1/Calculator.asmx";
-
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(endpoint);
-
-            request.ContentType = "text/xml"; // or application/soap+xml for SOAP 1.2
-            request.Method = "POST";
-            request.KeepAlive = false;
-
-            //In case you have a proxy to resolve the server name also add these lines
-            var proxyServer = new WebProxy("XX.XX.XX.XX", 1234);
-
-            proxyServer.Credentials = CredentialCache.DefaultCredentials; // or username + password
-
-            request.Proxy = proxyServer;
-
-            // you can read these from files
-            string payload = @"<soapenv:Envelope xmlns:soapenv=""http://schemas.xmlsoap.org/soap/envelope/"" xmlns:tem=""http://tempuri.org/"">
-                               <soapenv:Header/>
-                               <soapenv:Body>
-                                  <tem:Add>
-                                     <tem:a>1</tem:a>
-                                     <tem:b>2</tem:b>
-                                  </tem:Add>
-                               </soapenv:Body>
-                            </soapenv:Envelope>";
-
-            byte[] byteArray = Encoding.UTF8.GetBytes(payload);
-
-            request.ContentLength = byteArray.Length;
-
-            Stream requestStream = request.GetRequestStream();
-
-            requestStream.Write(byteArray, 0, byteArray.Length);
-            requestStream.Close();
-
-            HttpWebResponse response = null;
-            try
-            {
-                response = (HttpWebResponse)request.GetResponse();
-            }
-            catch (WebException ex)
-            {
-                response = (HttpWebResponse)ex.Response;
-            }
-
-            Console.WriteLine(string.Format("HTTP/{0} {1} {2}\n", response.ProtocolVersion, (int)response.StatusCode, response.StatusDescription));
-
-            // you can write this to files
-            Stream responseStream = response.GetResponseStream();
-            StreamReader reader = new StreamReader(responseStream);
-            Console.WriteLine(reader.ReadToEnd());
-
-            // cleanp
-            reader.Close();
-            requestStream.Close();
-            responseStream.Close();
-            response.Close();
+            //creating object of program class to access methods    
+            Program obj = new Program();
+            //Console.WriteLine("Please Enter Input values..");
+            ////Reading input values from console    
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //Calling InvokeService method    
+            obj.InvokeService();
         }
+
+
+        public HttpWebRequest CreateSOAPWebRequest()
+        {
+            //Making Web Request    
+            HttpWebRequest Req = (HttpWebRequest)WebRequest.Create(@"http://172.36.0.141/asdkws/User.asmx");
+            //SOAPAction    
+            Req.Headers.Add(@"SOAPAction:http://tempuri.org/ListPerfiles");
+            //Content_type    
+            Req.ContentType = "text/xml;charset=\"utf-8\"";
+            Req.Accept = "text/xml";
+            //HTTP method    
+            Req.Method = "POST";
+            //return HttpWebRequest    
+            return Req;
+        }
+
+
+        public void InvokeService()
+        {
+            //Calling CreateSOAPWebRequest method    
+            HttpWebRequest request = CreateSOAPWebRequest();
+
+            XmlDocument SOAPReqBody = new XmlDocument();
+            //SOAP Body Request    
+            SOAPReqBody.LoadXml(@"<?xml version=""1.0"" encoding=""utf-8""?>
+            <soap:Envelope xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"" xmlns:soap=""http://schemas.xmlsoap.org/soap/envelope/"">
+                <soap:Body>
+                    <ListPerfiles xmlns=""http://tempuri.org/"" />
+                </soap:Body>
+            </soap:Envelope>");    
+
+
+
+        using (Stream stream = request.GetRequestStream())
+            {
+                SOAPReqBody.Save(stream);
+            }
+            //Geting response from request    
+            using (WebResponse Serviceres = request.GetResponse())
+            {
+                using (StreamReader rd = new StreamReader(Serviceres.GetResponseStream()))
+                {
+                    //reading stream    
+                    var ServiceResult = rd.ReadToEnd();
+
+                    //XmlSerializer serializer
+                    XmlDocument xml = new XmlDocument();
+                    xml.LoadXml(ServiceResult);
+
+                    XmlNodeList xnList = xml.GetElementsByTagName("User");
+
+                    foreach (XmlNode xn in xnList)
+                    {
+                        string firstName = xn["Uname"].InnerText;
+                        string lastName = xn["UserActive"].InnerText;
+                        Console.WriteLine("Perfil: {0} {1}", firstName, lastName);
+                    }
+
+
+
+                    Console.WriteLine("Listo!");
+
+                    Console.ReadLine();
+                }
+            }
+
+
+
+        }
+
+
     }
 }
